80e46d8fa2888ba17a9ef08f38fa8c1e0ab63849
Johnatan <devsantosjohn@gmail.com>
2024-08-17 14:45:08 -0300

    :sparkles: [FEAT] - Import de axios funcionaa qui.

--- src/renderers/login/index.js
+++ src/renderers/login/index.js
@@ -1,10 +1,20 @@
+const { http } = require("../../providers/httpClient/axios")
+
 console.log('Script da página de login')
 
 function redirectToQrCode() {
+  const email = 'john@vendergas.com'
+  const senha = '123456'
 
-  console.log('redirecionando para a página de QR CODE')
-
-  const anchor = document.createElement('a')
-  anchor.setAttribute('href', '../qrcode/index.html')
-  anchor.click()
+  http.post('/usuarios/auth', {
+    email,
+    senha
+  }).then(() => {
+    alert('Logado com sucesso')
+    const anchor = document.createElement('a')
+    anchor.setAttribute('href', '../qrcode/index.html')
+    anchor.click()
+  }).catch((err) => {
+    alert(err.response.data.message || err.message)
+  })
 }
